f10c484f1184e0f30616a847ccdba6967163c1e9
Mohammad Asif <38504330+geekyasif@users.noreply.github.com>
2023-05-30 22:23:09 +0530

    Update firebase.js
    removed api key

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2oLz3Glkeml7Gmpb2dTbqIplbUjIRhGg",
+  apiKey: "",
   authDomain: "getmynotes-93864.firebaseapp.com",
   projectId: "getmynotes-93864",
   storageBucket: "getmynotes-93864.appspot.com",
-  messagingSenderId: "630284091260",
+  messagingSenderId: "",
   appId: "1:630284091260:web:efb3cb106760411a912ef9"
 };
 
