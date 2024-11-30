9948727fa2e20a06bdc57edff9d3141149b90043
Mohammad Asif <38504330+geekyasif@users.noreply.github.com>
2024-11-30 15:31:02 +0530

    Update firebase.js
    removed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2oLz3Glkeml7Gmpb2dTbqIplbUjIRhGg",
-  authDomain: "getmynotes-93864.firebaseapp.com",
-  projectId: "getmynotes-93864",
-  storageBucket: "getmynotes-93864.appspot.com",
-  messagingSenderId: "630284091260",
-  appId: "1:630284091260:web:efb3cb106760411a912ef9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
